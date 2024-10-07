cb14c9d6118e6fe0974a516134dfef1aeaef9b66
Julien Schueller <schueller@phimeca.com>
2024-10-07 22:10:24 +0200

    drop

--- sample/cppref-sample.cpp
+++ sample/cppref-sample.cpp
@@ -51,31 +51,9 @@ int main()
     // save state
     std::stringstream stream;
     stream << rng;
+    
+    std::cout << "state="<<stream.str()<<std::endl;
 
     //reload stream
     stream >> rng;
-
-    // Choose a random mean between 1 and 6
-    std::uniform_int_distribution<int> uniform_dist(1, 6);
-    int mean = uniform_dist(rng);
-    std::cout << "Randomly-chosen mean: " << mean << '\n';
- 
-    // Generate a normal distribution around that mean
-    std::normal_distribution<> normal_dist(mean, 2);
-    
-    // Make a copy of the RNG state to use later
-    pcg64 rng_checkpoint = rng;
- 
-    std::map<int, int> hist;
-    for (int n = 0; n < 10000; ++n) {
-        ++hist[std::round(normal_dist(rng))];
-    }
-    std::cout << "Normal distribution around " << mean << ":\n";
-    for (auto p : hist) {
-        std::cout << std::fixed << std::setprecision(1) << std::setw(2)
-                  << p.first << ' ' << std::string(p.second/30, '*')
-                  << '\n';
-    }
-    
-    std::cout << "Required " << (rng - rng_checkpoint) << " random numbers.\n";
 }
